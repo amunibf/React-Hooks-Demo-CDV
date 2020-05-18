b71ebe57d489555c62c2dbd25e9d7e5751d864b9
amunibf <amunibf@gmail.com>
2020-05-19 05:41:07 +0700

    useReducer - CounterOne.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import IntervalClassCounter from './components/IntervalClassCounter';
 // import IntervalHookCounter from './components/IntervalHookCounter';
 import DataFetching from './components/DataFetching';
 import ComponentC from './components/ComponentC';
+import CounterOne from './components/CounterOne';
 
 
 
@@ -21,7 +22,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">    
+      <div className="App"> 
+      <CounterOne/>
       {/* <DataFetching/> */}
       {/* <IntervalHookCounter/>
       <IntervalClassCounter/> */}
